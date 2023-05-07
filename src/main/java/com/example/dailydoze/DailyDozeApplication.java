baf68e501581505fae36d425df75ad317ec14c21
Astan Serikov <serikovastik@gmail.com>
2023-05-07 13:13:53 +0600

    Initial commit

---
+++ src/main/java/com/example/dailydoze/DailyDozeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailydoze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDozeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyDozeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailydoze/DailyDozeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailydoze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDozeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
